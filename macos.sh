1bc3583bd92443ad62622379cb0d2d6e60eb8888
johnche <johnche@tencent.com>
2022-07-25 17:58:11 +0800

    解决patch不生效的问题，并且把inspector改为导出

--- macos.sh
+++ macos.sh
@@ -11,12 +11,17 @@ git checkout v$VERSION
 
 echo "=====[Patching Node.js]====="
 
+node $GITHUB_WORKSPACE/CRLF2LF.js $GITHUB_WORKSPACE/nodemod.patch
 git apply --cached $GITHUB_WORKSPACE/nodemod.patch
+node $GITHUB_WORKSPACE/CRLF2LF.js $GITHUB_WORKSPACE/lib_uv_add_on_watcher_queue_updated.patch
 git apply --cached $GITHUB_WORKSPACE/lib_uv_add_on_watcher_queue_updated.patch
 git checkout -- .
 
 echo "=====[ add ArrayBuffer_New_Without_Stl ]====="
 node $GITHUB_WORKSPACE/add_arraybuffer_new_without_stl.js deps/v8
+
+echo "=====[ add make_v8_inspector_export ]====="
+node $GITHUB_WORKSPACE/make_v8_inspector_export.js
 
 echo "=====[Building Node.js]====="
 
@@ -26,11 +31,6 @@ make -j8
 mkdir -p ../puerts-node/nodejs/include
 mkdir -p ../puerts-node/nodejs/deps/uv/include
 mkdir -p ../puerts-node/nodejs/deps/v8/include
-
-cp src/node.h ../puerts-node/nodejs/include
-cp src/node_version.h ../puerts-node/nodejs/include
-cp -r deps/uv/include ../puerts-node/nodejs/deps/uv
-cp -r deps/v8/include ../puerts-node/nodejs/deps/v8
 
 mkdir -p ../puerts-node/nodejs/lib/macOS/
 cp out/Release/libnode.*.dylib ../puerts-node/nodejs/lib/macOS/
--- macos_arm64.sh
+++ macos_arm64.sh
@@ -11,12 +11,17 @@ git checkout v$VERSION
 
 echo "=====[Patching Node.js]====="
 
+node $GITHUB_WORKSPACE/CRLF2LF.js $GITHUB_WORKSPACE/nodemod.patch
 git apply --cached $GITHUB_WORKSPACE/nodemod.patch
+node $GITHUB_WORKSPACE/CRLF2LF.js $GITHUB_WORKSPACE/lib_uv_add_on_watcher_queue_updated.patch
 git apply --cached $GITHUB_WORKSPACE/lib_uv_add_on_watcher_queue_updated.patch
 git checkout -- .
 
 echo "=====[ add ArrayBuffer_New_Without_Stl ]====="
 node $GITHUB_WORKSPACE/add_arraybuffer_new_without_stl.js deps/v8
+
+echo "=====[ add make_v8_inspector_export ]====="
+node $GITHUB_WORKSPACE/make_v8_inspector_export.js
 
 echo "=====[Building Node.js]====="
 
