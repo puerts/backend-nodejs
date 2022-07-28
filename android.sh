b565ece334b13627f392dee64a30b9e0aa783dcd
johnche <johnche@tencent.com>
2022-07-28 18:41:57 +0800

    改为直接用wget下载nodejs源码包

--- android.sh
+++ android.sh
@@ -24,19 +24,10 @@ case $ARCH in
 esac
 
 cd ~
-git clone https://github.com/nodejs/node.git
+wget https://nodejs.org/download/release/v14.16.1/node-v14.16.1.tar.gz
+tar xvfz node-v14.16.1.tar.gz
 
-cd node
-git fetch origin v$VERSION
-git checkout v$VERSION
-
-echo "=====[Patching Node.js]====="
-
-node $GITHUB_WORKSPACE/CRLF2LF.js $GITHUB_WORKSPACE/nodemod.patch
-git apply --cached $GITHUB_WORKSPACE/nodemod.patch
-node $GITHUB_WORKSPACE/CRLF2LF.js $GITHUB_WORKSPACE/lib_uv_add_on_watcher_queue_updated.patch
-git apply --cached $GITHUB_WORKSPACE/lib_uv_add_on_watcher_queue_updated.patch
-git checkout -- .
+cd node-v14.16.1
 
 echo "=====[ add ArrayBuffer_New_Without_Stl ]====="
 node $GITHUB_WORKSPACE/add_arraybuffer_new_without_stl.js deps/v8
