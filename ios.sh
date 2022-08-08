48783d932e32faccf9595148f810a178a385a2eb
johnche <johnche@tencent.com>
2022-08-08 23:00:10 +0800

    missing handler_inside_posix

--- ios.sh
+++ ios.sh
@@ -12,6 +12,7 @@ git checkout v$VERSION
 
 echo "=====[Patching Node.js]====="
 node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/lib_uv_add_on_watcher_queue_updated_v$VERSION.patch
+node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/fix_no_handler_inside_posix_v$VERSION.patch
 node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/ios_ninja_compile_for_v$VERSION.patch
 node $WORKSPACE/node-script/add_arraybuffer_new_without_stl.js deps/v8
 node $WORKSPACE/node-script/make_v8_inspector_export.js
