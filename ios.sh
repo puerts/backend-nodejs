f7d9da9ebc95ba7f42960745a653ec02e7fdee58
johnche <johnche@tencent.com>
2022-08-09 12:25:31 +0800

    cp ngtcp2 if version > 16

--- ios.sh
+++ ios.sh
@@ -97,3 +97,44 @@ cp \
   out/Release/obj/tools/v8_gypfiles/libv8_compiler.a \
   out/Release/obj/tools/v8_gypfiles/libv8_initializers.a \
   ../puerts-node/nodejs/lib/iOS/
+
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+vercomp "16" $VERSION
+
+if [ $? == 2 ]
+then
+cp \
+  out/Release/obj/deps/ngtcp2/libngtcp2.a \
+  out/Release/obj/deps/ngtcp2/libnghttp3.a \
+  ../puerts-node/nodejs/lib/iOS/
+fi
