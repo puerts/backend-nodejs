6c30ded90f2b80bf9dc8fa5de660c733f6313c7f
johnche <johnche@tencent.com>
2022-09-02 12:01:02 +0800

    回退

--- android.sh
+++ android.sh
@@ -34,7 +34,6 @@ echo "=====[Patching Node.js]====="
 # node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/android_disable_alink_thin_v$VERSION.patch
 node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/lib_uv_add_on_watcher_queue_updated_v$VERSION.patch
 node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/fix_no_handler_inside_posix_v$VERSION.patch
-node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/win_build_v%VERSION%.patch
 node $WORKSPACE/node-script/add_arraybuffer_new_without_stl.js deps/v8
 node $WORKSPACE/node-script/make_v8_inspector_export.js
 
