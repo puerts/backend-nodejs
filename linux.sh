11092c1595459439572ea1e72554c9d263c9633e
johnche <johnche@tencent.com>
2023-02-16 10:29:56 +0800

    create symlink for linxu

--- linux.sh
+++ linux.sh
@@ -36,3 +36,6 @@ cp -r deps/v8/include ../puerts-node/nodejs/deps/v8
 
 mkdir -p ../puerts-node/nodejs/lib/Linux/
 cp out/Release/libnode.so.* ../puerts-node/nodejs/lib/Linux/
+cd ../puerts-node/nodejs/lib/Linux/
+ln -s libnode.so.93 libnode.so
+cd -
